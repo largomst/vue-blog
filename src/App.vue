4117da6bf2149a07880ee964c3b01c8bb097cd0d
largomst <dingrui.zhen@gmail.com>
2021-10-27 15:50:08 +0800

    add article list

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,51 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
+  <div class="text-center mt-5">
+    <h1 class="text-4xl font-semibold p-4">My Drf-Vue Blog</h1>
+    <hr />
   </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-3" v-for="article in articles" v-bind:key="article.url">
+    <div>
+      <span
+        class="px-1 py-0.5 mt-1 mr-1 mb-1 font-serif text-sm bg-gray-400 rounded text-gray-50"
+        v-for="tag in article.tags"
+        v-bind:key="tag"
+        >{{ tag }}</span
+      >
+    </div>
+    <div class="px-0 py-1  text-3xl  text-gray-800 font-semibold">
+      {{ article.title }}
+    </div>
+    <div class="text-sm">{{ formatted_time(article.created) }}</div>
+  </div>
+  <div
+    class="fixed left-0 bottom-0 h-12 w-full bg-gray-50 text-center font-serif"
+  >
+    <p>blog.kotens.xyz</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      articles: "",
+    };
+  },
+  methods: {
+    formatted_time: function(iso_date_string) {
+      const date = new Date(iso_date_string);
+      return date.toLocaleDateString();
+    },
+  },
+  mounted() {
+    axios
+      .get("/api/article/")
+      .then((response) => (this.articles = response.data));
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: `http://localhost:8000/api`,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+    },
+  },
+};
