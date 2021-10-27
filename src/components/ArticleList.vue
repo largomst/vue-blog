d705e9f28747e45d0794f42359b73afaaa02f53f
largomst <dingrui.zhen@gmail.com>
2021-10-27 23:20:17 +0800

    add pagenation

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -18,6 +18,24 @@
     </div>
     <div class="text-sm">{{ formatted_time(article.created) }}</div>
   </div>
+
+  <div>
+    <span v-if="is_page_exists('prev')">
+      <router-link
+        :to="{ name: 'Home', query: { page: get_page_param('prev') } }"
+      >
+        Prev
+      </router-link>
+    </span>
+    <span>{{ get_page_param("current") }}</span>
+    <span v-if="is_page_exists('next')">
+      <router-link
+        :to="{ name: 'Home', query: { page: get_page_param('next') } }"
+      >
+        Next
+      </router-link>
+    </span>
+  </div>
 </template>
 
 <script>
@@ -27,6 +45,7 @@ export default {
   data() {
     return {
       articles: "",
+      link: { next: null, prev: null },
     };
   },
   methods: {
@@ -34,11 +53,72 @@ export default {
       const date = new Date(iso_date_string);
       return date.toLocaleDateString();
     },
+    extract_prev_next_from_headers_link(link) {
+      const nextPattern = /rel="next"/;
+      const prevPattern = /rel="prev"/;
+      const linkPattern = /(?<=<)(.*)(?=>)/;
+      let links = [];
+      if (link !== undefined) {
+        links = link.split(",");
+        this.link = { next: null, prev: null };
+        links.forEach((str) => {
+          if (nextPattern.test(str)) {
+            this.link.next = str.match(linkPattern)[0];
+          }
+          if (prevPattern.test(str)) {
+            this.link.prev = str.match(linkPattern)[0];
+          }
+        });
+        // console.log(this.link);
+      }
+    },
+    get_article_data() {
+      let url = "/api/article/";
+      const page = Number(this.$route.query.page);
+      if (!isNaN(page) && page !== 0) {
+        url = url + "?page=" + page;
+      }
+      axios.get(url).then((response) => {
+        this.articles = response.data;
+        this.extract_prev_next_from_headers_link(response.headers.link);
+      });
+    },
+    is_page_exists(direction) {
+      if (direction == "next") {
+        return this.link.next !== null;
+      } else {
+        return this.link.prev !== null;
+      }
+    },
+    get_page_param(direction) {
+      try {
+        let url_string;
+        switch (direction) {
+          case "next":
+            url_string = this.link.next;
+            break;
+          case "prev":
+            url_string = this.link.prev;
+            break;
+          default:
+            return this.$route.query.page;
+        }
+        const url = new URL(url_string);
+        console.log(url.searchParams.get("page"));
+        return url.searchParams.get("page");
+      } catch (err) {
+        console.log(err);
+        return;
+      }
+    },
   },
   mounted() {
-    axios
-      .get("/api/article/")
-      .then((response) => (this.articles = response.data));
+    this.get_article_data();
+  },
+  watch: {
+    $route() {
+      this.get_article_data();
+    },
   },
 };
 </script>
