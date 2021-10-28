d87d6ba495feced941c3b7dbca8aec87a5220b84
largomst <dingrui.zhen@gmail.com>
2021-10-28 09:33:28 +0800

    add search bar

--- src/components/BlogHeader.vue
+++ src/components/BlogHeader.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="text-center mt-5">
-    <h1 class="text-4xl font-semibold p-4">My Drf-Vue Blog</h1>
-    <hr />
+  <div class="grid grid-cols-4 text-center mt-5">
+    <div></div>
+    <h1 class="col-start-2 col-end-4 text-4xl font-semibold p-4">
+      My Drf-Vue Blog
+    </h1>
+    <div class="pt-5">
+      <form class="flex" action="">
+        <input
+          class="flex-grow p-2 w-20 rounded-l-md border-2 border-gray-400 outline-none"
+          type="text"
+          placeholder="请输入搜索"
+        />
+        <button
+          class="flex-grow-0 p-2 cursor-pointer bg-gray-400 rounded-r-md text-white whitespace-nowrap"
+        >
+          搜索
+        </button>
+      </form>
+    </div>
   </div>
+  <hr />
 </template>
